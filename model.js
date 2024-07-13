0ed573709818299f12b571e8c32b873165ad41e6
GammaBeta <43095444+GammaBeta@users.noreply.github.com>
2024-07-12 21:56:38 -0300

    Update model.js

--- model.js
+++ model.js
@@ -56,7 +56,7 @@ const Npcs = {
     Archmage: 'Archmage',
     BrimstoneWitch: 'Brimstone Witch',
     //Fargo's Mod
-    Lumberjack: 'Lumberjack',
+    LumberJack: 'LumberJack',
     Deviantt: 'Deviantt',
     Abominationn: 'Abominationn',
     Mutant: 'Mutant',
@@ -126,7 +126,7 @@ const NpcModels = [
     new NpcBuilder(Npcs.Archmage).likes(Biomes.Snow, Npcs.Wizard, Npcs.Princess).dislikes(Biomes.Desert, Npcs.Cyborg).build(),
     new NpcBuilder(Npcs.BrimstoneWitch).likes(Biomes.Forest, Npcs.Clothier, Npcs.Princess).dislikes(Biomes.BrimstoneCrag, Npcs.PartyGirl).build(),
     //Fargo's Mod
-    new NpcBuilder(Npcs.Lumberjack).loves(Biomes.Forest).likes(Npcs.Squirrel, Npcs.Princess).dislikes(Npcs.Dryad).hates(Npcs.Demolitionist).build(),
+    new NpcBuilder(Npcs.LumberJack).loves(Biomes.Forest).likes(Npcs.Squirrel, Npcs.Princess).dislikes(Npcs.Dryad).hates(Npcs.Demolitionist).build(),
     new NpcBuilder(Npcs.Deviantt).loves(Biomes.Space, Npcs.Mutant).likes(Biomes.Jungle, Npcs.Abominationn, Npcs.Princess).dislikes(Biomes.Snow, Npcs.Zoologist).hates(Biomes.Desert, Npcs.Angler).build(),
     new NpcBuilder(Npcs.Abominationn).loves(Biomes.Space, Npcs.Mutant).likes(Biomes.Ocean, Npcs.Deviantt, Npcs.Princess).dislikes(Biomes.TheDungeon).hates(Npcs.Nurse).build(),
     new NpcBuilder(Npcs.Mutant).loves(Biomes.Space, Npcs.Abominationn).likes(Biomes.Forest, Npcs.Deviantt, Npcs.Princess).dislikes(Biomes.Hallow, Npcs.Lumberjack).build(),
