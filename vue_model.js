6b83c620a812bf27ec7f8c9f69fc51c136203806
Minerofmillions <jasoncollins818@gmail.com>
2024-07-19 21:49:45 -0400

    NPC selection dropdown stays the same size

--- vue_model.js
+++ vue_model.js
@@ -1,46 +1,46 @@
 Vue.component("town", {
   template: `
     <div class="ui-frame town">
-        <div style='display: flex; justify-content: space-evenly' :style="{width: townUIWidth + 'px'}">
-            <img width="44px" v-for='biome in availableBiomes' :src="'images/' + biome + '.png'" :title="biome" :class="{ disabled: !biomes.includes(biome) }" v-on:click="toggleBiome(biome)" style="cursor: pointer;" />
+      <div style='display: flex; justify-content: space-evenly' :style="{width: townUIWidth + 'px'}">
+        <img width="44px" v-for='biome in availableBiomes' :src="'images/' + biome + '.png'" :title="biome" :class="{ disabled: !biomes.includes(biome) }" v-on:click="toggleBiome(biome)" style="cursor: pointer;" />
             
-            <select name="npcs" v-model='selectedNpc' style='margin-left: 44px'>
-                <optgroup v-for='(npcs, group) in availableNpcs' :label="group">
-                    <option v-for='npc in npcs' :value="npc">{{npc.Name}}</option>
-                </optgroup>
-            </select>
-            <button v-on:click='addSelectedNpc()'>Add NPC</button>
-            <!--<button @click='removeTown'>Remove Town</button>-->
-        </div>
+        <select name="npcs" v-model='selectedNpc' style='margin-left: 44px; width: 150px;'>
+          <optgroup v-for='(npcs, group) in availableNpcs' :label="group">
+            <option v-for='npc in npcs' :value="npc">{{npc.Name}}</option>
+          </optgroup>
+        </select>
+        <button v-on:click='addSelectedNpc()'>Add NPC</button>
+        <!--<button @click='removeTown'>Remove Town</button>-->
+      </div>
         
-        <div style="display: flex; flex-direction: row; flex-wrap: wrap; min-height: 105px;">
-            <div class="npc" v-for='npc in npcs'>
-              <div v-if='npc.Name != "Princess"'>
-                <img :src="'images/' + npc.Name.replace(' ', '') + '.png'" />
-                <div class="name">{{npc.Name}}</div>
-                <div :class="{ happy: happiness[npc.Name] <= 90, sad: happiness[npc.Name] > 100 }">{{happiness[npc.Name]}}%</div>
-                <div class="remove" v-on:click="removeNpc(npc)" title="Remove from town">X</div>
-                <div class="tooltip">
-                  <div>Loves: <img v-for="love in npc.Loved" :src="'images/emotes/'+love.replace(' ', '')+'.png'" :title="love"></div>
-                  <div>Likes: <img v-for="like in npc.Liked" :src="'images/emotes/'+like.replace(' ', '')+'.png'" :title="like"></div>
-                  <div>Dislikes: <img v-for="dislike in npc.Disliked" :src="'images/emotes/'+dislike.replace(' ', '')+'.png'" :title="dislike"></div>
-                  <div>Hates: <img v-for="hate in npc.Hated" :src="'images/emotes/'+hate.replace(' ', '')+'.png'" :title="hate"></div>
-                </div>
-              </div>
-              <div v-else>
-                <img :src="'images/' + npc.Name.replace(' ', '') + '.png'" />
-                <div class="name">{{npc.Name}}</div>
-                <div :class="{ happy: happiness[npc.Name] <= 90, sad: happiness[npc.Name] > 100 }">{{happiness[npc.Name]}}%</div>
-                <div class="remove" v-on:click="removeNpc(npc)" title="Remove from town">X</div>
-                <div class="tooltip" style="width: 111px;">
-                  <div>Loves: Every NPC</div>
-                  <div>Likes: </div>
-                  <div>Dislikes: </div>
-                  <div>Hates: </div>
-                </div>
-          </div>
+      <div style="display: flex; flex-direction: row; flex-wrap: wrap; min-height: 105px;">
+        <div class="npc" v-for='npc in npcs'>
+          <div v-if='npc.Name != "Princess"'>
+            <img :src="'images/' + npc.Name.replace(' ', '') + '.png'" />
+            <div class="name">{{npc.Name}}</div>
+            <div :class="{ happy: happiness[npc.Name] <= 90, sad: happiness[npc.Name] > 100 }">{{happiness[npc.Name]}}%</div>
+            <div class="remove" v-on:click="removeNpc(npc)" title="Remove from town">X</div>
+            <div class="tooltip">
+              <div>Loves: <img v-for="love in npc.Loved" :src="'images/emotes/'+love.replace(' ', '')+'.png'" :title="love"></div>
+              <div>Likes: <img v-for="like in npc.Liked" :src="'images/emotes/'+like.replace(' ', '')+'.png'" :title="like"></div>
+              <div>Dislikes: <img v-for="dislike in npc.Disliked" :src="'images/emotes/'+dislike.replace(' ', '')+'.png'" :title="dislike"></div>
+              <div>Hates: <img v-for="hate in npc.Hated" :src="'images/emotes/'+hate.replace(' ', '')+'.png'" :title="hate"></div>
             </div>
+          </div>
+          <div v-else>
+            <img :src="'images/' + npc.Name.replace(' ', '') + '.png'" />
+            <div class="name">{{npc.Name}}</div>
+            <div :class="{ happy: happiness[npc.Name] <= 90, sad: happiness[npc.Name] > 100 }">{{happiness[npc.Name]}}%</div>
+            <div class="remove" v-on:click="removeNpc(npc)" title="Remove from town">X</div>
+            <div class="tooltip" style="width: 111px;">
+              <div>Loves: Every NPC</div>
+              <div>Likes: </div>
+              <div>Dislikes: </div>
+              <div>Hates: </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
     `,
   created() {
